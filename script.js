8fba5d9acf1589a18ebe6f4d445cbf24b2724cb3
Mariana Mandziuk <madzukmar@gmail.com>
2025-10-22 00:19:51 +0300

    Create weather logic core

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 "use strict";
 
-const year = document.querySelector(".year"),
-  month = document.querySelector(".month"),
-  numberDay = document.querySelector(".number-day"),
-  day = document.querySelector(".day"),
-  minutes = document.querySelector(".minutes"),
-  hours = document.querySelector(".hours");
+const elements = {
+  year: document.querySelector(".year"),
+  month: document.querySelector(".month"),
+  numberDay: document.querySelector(".number-day"),
+  day: document.querySelector(".day"),
+  minutes: document.querySelector(".minutes"),
+  hours: document.querySelector(".hours"),
+  input: document.getElementById("site-search"),
+  inputSearch: document.querySelector(".search-icon"),
+  locationTitle: document.querySelector(".city"),
+  tempDegree: document.querySelector(".num-degree"),
+  tempMax: document.querySelector(".temp-max"),
+  tempMin: document.querySelector(".temp-min"),
+  humidity: document.querySelector(".humadity"),
+  cloudy: document.querySelector(".cloudy"),
+  wind: document.querySelector(".wind"),
+  descriptionWeather: document.querySelector(".content-title"),
+  weatherIcon: document.querySelector(".weather-icon"),
+};
 
 const days = [
   "Sunday",
@@ -32,11 +45,61 @@ const months = [
   "December",
 ];
 
-let now = new Date();
+const updateDateTime = () => {
+  const now = new Date();
+  elements.hours.textContent = now.getHours();
+  elements.minutes.textContent = now.getMinutes();
+  elements.numberDay.textContent = now.getDate();
+  elements.day.textContent = days[now.getDay()];
+  elements.month.textContent = months[now.getMonth()].slice(0, 3);
+  elements.year.textContent = now.getFullYear().toString().slice(-2);
+};
 
-hours.textContent = now.getHours();
-minutes.textContent = now.getMinutes();
-numberDay.textContent = now.getDate();
-day.textContent = days[now.getDay()];
-month.textContent = months[now.getMonth()].slice(0, 3);
-year.textContent = now.getFullYear().toString().slice(-2);
+const fetchJSON = (url) => fetch(url).then((response) => response.json());
+
+const getSearchLocation = async (value) => {
+  try {
+    const data = await fetchJSON(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=7191a96127dfbce98d4bf0728f9220a7`
+    );
+    const location = data[0];
+    elements.locationTitle.textContent = location.name;
+    getCurrentWeather(location.lat, location.lon);
+  } catch (error) {
+    console.error("Error fetching location data:", error);
+  }
+};
+
+const getCurrentWeather = async (lat, lon) => {
+  try {
+    const data = await fetchJSON(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7191a96127dfbce98d4bf0728f9220a7&units=metric`
+    );
+    elements.tempDegree.textContent = Math.round(data.main.temp);
+    elements.tempMax.textContent = Math.round(data.main.temp_max);
+    elements.tempMin.textContent = Math.round(data.main.temp_min);
+    elements.humidity.textContent = data.main.humidity;
+    elements.cloudy.textContent = data.clouds.all;
+    elements.wind.textContent = data.wind.speed;
+    elements.descriptionWeather.textContent = data.weather[0].description;
+    elements.weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  } catch (error) {
+    console.error("Error fetching location data:", error);
+  }
+};
+
+elements.input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const searchLocationInput = elements.input.value;
+    elements.input.value = "";
+    getSearchLocation(searchLocationInput);
+  }
+});
+
+elements.inputSearch.addEventListener("click", () => {
+  const searchLocationInput = elements.input.value;
+  elements.input.value = "";
+  getSearchLocation(searchLocationInput);
+});
+
+updateDateTime();
